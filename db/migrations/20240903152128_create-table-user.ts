92c2878eb207f7573a37863915d6a29b281c44b7
Alex Marques Beraldo <alexmarquesberaldo@gmail.com>
2024-09-03 18:31:13 -0300

    Creating users + meals tables with knex migrations

--- db/migrations/20240903152128_create-table-user.ts
+++ db/migrations/20240903152128_create-table-user.ts
@@ -3,13 +3,15 @@ import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
     await knex.schema.createTable("tb_users", (table) => {
-        table.uuid('id').primary()
+        table.increments('id').primary()
+        table.string('name')
         table.string('email').unique()
-        
+        table.uuid('session_id')
     })
 }
 
 
 export async function down(knex: Knex): Promise<void> {
+    knex.schema.dropTable("tb_users")
 }
 
---
+++ db/migrations/20240903212116_create-table-meals.ts
@@ -0,0 +1,24 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable("tb_meals", (table) => {
+        table.string('name').unique()
+
+        table.integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('tb_users')
+        .notNullable()
+
+        table.string('description')
+        table.timestamp('made_at')
+        table.boolean('in_diet')
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable("tb_meals")
+}
+
